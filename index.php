6d041cefd00ddd1b8ad8a31870a1fbcbec325c1a
nickdaggett <62954501+nickdaggett@users.noreply.github.com>
2020-04-09 14:33:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,25 @@
 			
 			}
 		</script>
-
+ <body>
+        <p>
+            <textarea id="top-text"></textarea>
+            Text size: <input type="range" id="top-text-size-input" min="0.05" max="0.25" value="0.15" step="0.01">
+        </p>
+        <p>
+            <textarea id="bottom-text"></textarea>
+            Text size: <input type="range" id="bottom-text-size-input" min="0.05" max="0.25" value="0.15" step="0.01">
+        </p>
+        <p>
+            <input type="file" id="image-input" accept="image/*">
+        </p>
+        <p>
+            <button id="generate-btn">Generate!</button>
+        </p>
+        <p>
+            <canvas id="meme-canvas" title="Right click -> &quot;Save image as...&quot;"></canvas>
+        </p>
+        <script src="memegen.js"></script>
+    </body>
 
 </html>
