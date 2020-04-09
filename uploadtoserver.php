e8363f594387d280c77340cd534561eb90d2b80e
nickdaggett <62954501+nickdaggett@users.noreply.github.com>
2020-04-09 12:37:30 -0400

    Delete uploadtoserver.php

--- uploadtoserver.php
+++
@@ -1,7 +0,0 @@
-$upload_dir = "../images/";
-$img = $_POST['data'];
-$img = str_replace('data:image/png;base64,', '', $img);
-$img = str_replace(' ', '+', $img);
-$data = base64_decode($img);
-$file = $upload_dir."image_name.png";
-file_put_contents($file, $data);
